cfb70521d99e85d75ad2f88c67a96698bef5499d
SAVITHA RAMESH <72896893+savram674@users.noreply.github.com>
2025-10-10 15:06:21 +0530

    Update App.java

--- src/main/java/in/kmit/demoE31/App.java
+++ src/main/java/in/kmit/demoE31/App.java
@@ -6,5 +6,6 @@ package in.kmit.demoE31;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hi Welcome to Elite!");
+         System.out.println("we are tryig webhooks!");
     }
 }
